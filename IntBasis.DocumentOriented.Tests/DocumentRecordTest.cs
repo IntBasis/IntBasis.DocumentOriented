8d2808625d85a3d914732e55175cfd7de6d993ef
Jacob Foshee <jacobf@gmail.com>
2022-06-10 15:14:18 -0500

    test: Use fluent assertions

--- IntBasis.DocumentOriented.Tests/DocumentRecordTest.cs
+++ IntBasis.DocumentOriented.Tests/DocumentRecordTest.cs
@@ -5,6 +5,11 @@ public record DocumentRecord(string Id) : IDocumentEntity;
 public record SchemaVersionedRecord(string Id) : ISchemaVersionedDocumentEntity
 {
     public int SchemaVersion { get; set; }
+
+    public SchemaVersionedRecord(string id, int schemaVersion) : this(id)
+    {
+        SchemaVersion = schemaVersion;
+    }
 }
 
 public class DocumentRecordTest
@@ -13,7 +18,11 @@ public class DocumentRecordTest
     public void ConstructDocumentRecord()
     {
         // Verify a `record` can implement our interfaces
-        new DocumentRecord("1");
-        new SchemaVersionedRecord("2");
+        var documentRecord = new DocumentRecord("1");
+        documentRecord.Id.Should().Be("1");
+
+        var schemaVersionedRecord = new SchemaVersionedRecord("2", 42);
+        schemaVersionedRecord.Id.Should().Be("2");
+        schemaVersionedRecord.SchemaVersion.Should().Be(42);
     }
 }
