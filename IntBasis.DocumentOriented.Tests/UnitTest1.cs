0ae91dab4b9f15e6aa7c8a3a15b660e5cc46a033
Jacob Foshee <jacobf@gmail.com>
2022-06-10 14:23:19 -0500

    test: Add project reference

--- IntBasis.DocumentOriented.Tests/UnitTest1.cs
+++ IntBasis.DocumentOriented.Tests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace IntBasis.DocumentOriented.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace IntBasis.DocumentOriented.Tests;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        new Class1();
     }
 }
