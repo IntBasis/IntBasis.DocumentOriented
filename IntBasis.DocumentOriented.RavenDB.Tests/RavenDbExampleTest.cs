f7e819578b26e9d16375442db4cc46d6be54c4c4
Jacob Foshee <jacobf@gmail.com>
2022-07-02 14:24:09 -0500

    test(ravendb): Verify `dynamic` support

--- IntBasis.DocumentOriented.RavenDB.Tests/RavenDbExampleTest.cs
+++ IntBasis.DocumentOriented.RavenDB.Tests/RavenDbExampleTest.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
 using Raven.Client.ServerWide;
@@ -120,4 +121,37 @@ public class RavenDbExampleTest
 
         session.SaveChanges();
     }
+
+    class HasDynamic : IDocumentEntity
+    {
+        public string? Id { get; set; }
+        public dynamic? Metadata { get; set; } = new ExpandoObject();
+    }
+
+    [Theory(DisplayName = "Store Dynamic"), Integration]
+    public async Task StoreDynamic(RavenDbDocumentStorage subject)
+    {
+        var entity = new HasDynamic
+        {
+            Metadata = new
+            {
+                Location = "Houston"
+            }
+        };
+        // entity.Metadata.Value = 42;
+        ((string)entity.Metadata.Location).Should().Be("Houston");
+        
+        await subject.Store(entity);
+        entity.Id.Should().NotBeNull();
+        var retrieved = await subject.Retrieve<HasDynamic>(entity.Id);
+
+        ((string?)retrieved.Metadata?.Location).Should().Be("Houston");
+
+        retrieved.Metadata.Value = 42;
+        await subject.Store(retrieved);
+        var retrieved2 = await subject.Retrieve<HasDynamic>(entity.Id);
+
+        ((string?)retrieved2.Metadata?.Location).Should().Be("Houston");
+        ((int?)retrieved2.Metadata?.Value).Should().Be(42);
+    }    
 }
