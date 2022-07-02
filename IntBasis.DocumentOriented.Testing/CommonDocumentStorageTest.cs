0bcb53831ab82e1f6316ce110eff41609516add8
Jacob Foshee <jacobf@gmail.com>
2022-07-02 16:41:51 -0500

    refactor(test): Extract common `dynamic` verification

---
+++ IntBasis.DocumentOriented.Testing/CommonDocumentStorageTest.cs
@@ -0,0 +1,40 @@
+using System.Dynamic;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace IntBasis.DocumentOriented.Testing;
+
+public class HasDynamic : IDocumentEntity
+{
+    public string? Id { get; set; }
+    public dynamic? Metadata { get; set; } = new ExpandoObject();
+}
+
+public static class CommonDocumentStorageTest
+{
+    public static async Task VerifyDynamicObjectStorage(IDocumentStorage subject)
+    {
+        var entity = new HasDynamic
+        {
+            Metadata = new
+            {
+                Location = "Houston"
+            }
+        };
+        // entity.Metadata.Value = 42;
+        ((string)entity.Metadata.Location).Should().Be("Houston");
+
+        await subject.Store(entity);
+        entity.Id.Should().NotBeNull();
+        var retrieved = await subject.Retrieve<HasDynamic>(entity.Id);
+
+        ((string?)retrieved.Metadata?.Location).Should().Be("Houston");
+
+        retrieved.Metadata.Value = 42;
+        await subject.Store(retrieved);
+        var retrieved2 = await subject.Retrieve<HasDynamic>(entity.Id);
+
+        ((string?)retrieved2.Metadata?.Location).Should().Be("Houston");
+        ((int?)retrieved2.Metadata?.Value).Should().Be(42);
+    }
+}
